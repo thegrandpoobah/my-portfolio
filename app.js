74c2ab2351d83d281adc38098227c9cee8e0bf5d
Sahab Yazdani <sahab.yazdani@gmail.com>
2016-01-02 16:23:42 -0500

    Second check-in.

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ var url = require('url')
 
 authorization = JSON.parse(fs.readFileSync('authorization.json', 'utf8'));
   
-function qtAuthorize(authorization) {
+function qtAuthorize(auth) {
 	return new Promise(function(resolve, reject) {
-		if (!authorization.generation_time || moment().diff(moment(authorization.generation_time, moment.ISO_8601), 'seconds') > authorization.expires_in) {
+		if (!auth.generation_time || moment().diff(moment(auth.generation_time, moment.ISO_8601), 'seconds') > auth.expires_in) {
 			console.log('Requesting Authorization from Questrade')
 			
 			// authorizing request
 			var opts = {
 				host: 'login.questrade.com',
-				path: '/oauth2/token?grant_type=refresh_token&refresh_token='+authorization.refresh_token,
+				path: '/oauth2/token?grant_type=refresh_token&refresh_token='+auth.refresh_token,
 				method: 'GET',
 				headers: {
 					'Content-Type': 'application/json'
@@ -36,6 +36,7 @@ function qtAuthorize(authorization) {
 					
 					var serverUrl = url.parse(a.api_server)
 					a.api_server = serverUrl.host
+					authorization = a
 					
 					fs.writeFile('authorization.json', JSON.stringify(a), function(err) {
 						if (err) {
@@ -52,24 +53,23 @@ function qtAuthorize(authorization) {
 			})
 			
 			req.end()
-			// get a new one
 		} else {
-			resolve(authorization)
+			resolve(auth)
 		}
 	})
 }
 
-function qtRequest(authorization, endpoint) {
+function qtRequest(auth, endpoint) {
 	return new Promise(function(resolve, reject) {
 		console.log('Making Data Request to Questrade', endpoint)
 		
 		var opts = {
-			host: authorization.api_server,
+			host: auth.api_server,
 			path: endpoint,
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': authorization.token_type + " " + authorization.access_token
+				'Authorization': auth.token_type + " " + auth.access_token
 			}
 		}
 		
@@ -89,41 +89,7 @@ function qtRequest(authorization, endpoint) {
 			})
 		})
 		
-		req.end()	
-	})
-}
-
-function staticRequest(endpoint) {
-	return new Promise(function(resolve, reject) {
-		console.log('Making Data Request to Questrade', endpoint)
-		
-		var opts = {
-			host: authorization.api_server,
-			path: endpoint,
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': authorization.token_type + " " + authorization.access_token
-			}
-		}
-		
-		var req = https.request(opts, function(res) {
-			var responseString = ''
-			
-			res.on('data', function(data) {
-				responseString += data
-			})
-			
-			res.on('end', function() {
-				resolve(responseString)
-			})
-			
-			res.on('error', function(e) {
-				reject(e)
-			})
-		})
-		
-		req.end()	
+		req.end()
 	})
 }
 
@@ -141,10 +107,6 @@ app.get('/api/*', function(req, res) {
 })
 app.use(express.static('dist'))
 
-//qtLogin().then(function(au) {
-// console.log(au) 
-//})
-
 app.set('etag', false)
 var server = app.listen(3000, function () {
   var host = server.address().address
@@ -152,8 +114,3 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port)
 })
-
-
-//qtRequest(authorization, '/v1/markets/candles/1897759?startTime=2015-01-01T00:00:00-05:00&endTime=2015-12-31T23:59:59-05:00&interval=OneDay').then(function(data) {
-//	console.log(data)
-//})
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,129 @@
-function qtRequest(endpoint) {
-  return $.getJSON(endpoint)
+var cache = {}
+
+function onSymbolLoadComplete(positions, balances) {
+	var byCurrency = {}
+	var byExchange = {}
+	
+	var cash = {}
+	_.each(balances.perCurrencyBalances, function(balance) {
+		balance.portfolioWeight = balance.cash / balance.marketValue
+		balance.cost = 0
+		balance.openPnl = 0
+		balance.percentageOpenPnl = 0
+		cash[balance.currency] = balance
+	})
+	
+	_.each(positions, function(position) {
+		if (_.isUndefined(position.currency)) {
+			return
+		}
+		
+		if (_.isUndefined(byCurrency[position.currency])) {
+			byCurrency[position.currency] = []
+		}
+		
+		position.percentageOpenPnl = position.openPnl / position.totalCost
+		position.percentageClosedPnl = position.closedPnl / position.totalCost
+		position.portfolioWeight = position.currentMarketValue / cash[position.currency].marketValue 
+		
+		cash[position.currency].cost += position.totalCost
+		cash[position.currency].openPnl = cash[position.currency].marketValue - cash[position.currency].cost
+		cash[position.currency].percentageOpenPnl = cash[position.currency].openPnl / cash[position.currency].cost
+		
+		byCurrency[position.currency].push(position)
+	})
+	
+	byCurrency['CAD'] = _.sortByOrder(byCurrency['CAD'], ['portfolioWeight'], ['desc'])
+	byCurrency['USD'] = _.sortByOrder(byCurrency['USD'], ['portfolioWeight'], ['desc'])
+	
+	_.each(positions, function(position) {
+		if (_.isUndefined(position.listingExchange)) {
+			return
+		}
+		
+		if (_.isUndefined(byExchange[position.listingExchange])) {
+			byExchange[position.listingExchange] = []
+		}
+		
+		byExchange[position.listingExchange].push(position)
+	})
+	
+var tblTemplate = Handlebars.compile('<table class="table table-condensed table-hover"><thead><tr><th>Symbol</th><th>Portfolio Weight</th><th>Shares</th><th>Avg Buy Price</th><th>Cost</th><th>Current Price</th><th>Market Value</th><th>Unrealized Gain/Loss (%)</th><th>Realized Gain/Loss (%)</th></tr></thead><tbody>{{#positions}}<tr><td><a href="#" data-symbolId="{{symbolId}}"">{{symbol}}</a></td><td>{{percentage portfolioWeight}}</td><td>{{openQuantity}}</td><td>{{currency averageEntryPrice}}</td><td>{{currency totalCost}}</td><td>{{currency currentPrice}}</td><td>{{currency currentMarketValue}}</td><td class="{{currencyClass openPnl}}">{{currency openPnl}} ({{percentage percentageOpenPnl}}%)</td><td class="{{currencyClass closedPnl}}">{{currency closedPnl}} ({{percentage percentageClosedPnl}}%)</td></tr>{{/positions}}<tr><td>Cash</td><td>{{percentage balance.portfolioWeight}}</td><td></td><td></td><td></td><td></td><td>{{currency balance.cash}}</td><td></td><td></td></tr></tr><tr><td>Total Market Value</td><td></td><td></td><td></td><td>{{currency balance.cost}}</td><td></td><td>{{currency balance.marketValue}}</td><td class="{{currencyClass balance.openPnl}}">{{currency balance.openPnl}} ({{percentage balance.percentageOpenPnl}}%)</td><td></td></tr></tbody></table>')
+	$('#cadPositions').html(tblTemplate({positions: byCurrency['CAD'], balance: cash['CAD']}))
+	$('#usdPositions').html(tblTemplate({positions: byCurrency['USD'], balance: cash['USD']}))
+	
+	cache.positions = positions
+	cache.balances = balances
 }
 
 $(function() {
-  qtRequest('/api/accounts').done(function(ss) {
-  //qtRequest('/v1/markets/candles/1897759?startTime=2015-01-01T00:00:00-05:00&endTime=2015-12-31T23:59:59-05:00&interval=OneDay').then(function(ss) {
-  //  console.log('get here')
-    console.log(ss)
-  }).error(function(e) { /*console.logfdds(e)*/ })
+	Handlebars.registerHelper({
+		'currency': function(amount) {
+			if (amount >= 0) {
+				return amount.toFixed(2)
+			} else {
+				return '(' + amount.toFixed(2) + ')'
+			}
+		},
+		'currencyClass': function(amount) {
+			if (Math.abs(amount) < 0.001) {
+				return 'currency-zero'
+			} else if (amount >= 0) {
+				return 'currency-positive'
+			} else {
+				return 'currency-negative'
+			}
+		},
+		'percentage': function(amount) {
+			return (amount * 100).toFixed(2)
+		}
+	})
+	
+	$.getJSON('/api/accounts').then(function(resp) {
+		var accountId = resp.accounts[0].number
+		
+		$.getJSON('/api/accounts/' + accountId + '/balances').then(function(resp) {
+			var balances = resp
+			
+			$.getJSON('/api/accounts/' + accountId + '/positions').then(function(resp) {
+				var index = 0
+				var positions = resp.positions
+				
+				onSymbolLoadComplete(positions, balances)
+				
+				function enhanceSymbolInfo() {
+					if (index >= positions.length) {
+						return
+					}
+					
+					var position = positions[index]
+					index++
+					
+					$.getJSON('/api/symbols/' + position.symbolId).then(function(resp) {
+						_.merge(position, resp.symbols[0])
+						
+						onSymbolLoadComplete(positions, balances)
+					})
+					
+					window.setTimeout(enhanceSymbolInfo, 300)
+				}
+				
+				window.setTimeout(enhanceSymbolInfo, 300)
+			})
+		})
+	})
+	
+	$('.position-container a').on('click', function(e) {
+		console.log(e)
+	})
+	
+//	$.getJSON('/api/symbols/search?prefix=TSX').then(function(ss) {
+//		return $.getJSON('/api/markets/candles/' + ss.symbols[0].symbolId + '?startTime=2015-01-01T00:00:00-05:00&endTime=2015-12-31T23:59:59-05:00&interval=OneDay')
+//	}).then(function(qq) {
+//		console.log(qq)
+//	})
+//  qtRequest('/api/markets/candles/1897759?startTime=2015-01-01T00:00:00-05:00&endTime=2015-12-31T23:59:59-05:00&interval=OneDay').done(function(ss) {
+  //qtRequest().then(function(ss) {
+  //  console.log(ss)
+  //}).error(function(e) { /*console.logfdds(e)*/ })
 })
