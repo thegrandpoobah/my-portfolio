bf59425723128cbcdcbeb0256dee9e07f5f5b2f0
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2016-08-28 09:27:09 -0400

    Change name of distribution file from latest.zip to server.zip

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ cp ./server/questrade.js ./dist/questrade.js
 cp ./server/server.js ./dist/server.js
 mkdir -p ./dist/cron && cp ./server/cron/storeaccountmv.js ./dist/cron/storeaccountmv.js
 cp -R ./server/config ./dist/config
-(cd ./dist && zip -r ../latest.zip ./*)
-aws s3 cp --region us-east-1 ./latest.zip s3://my-portfolio-deploy
+(cd ./dist && zip -r ../server.zip ./*)
+aws s3 cp --region us-east-1 ./server.zip s3://my-portfolio-deploy
