30fdbbcaace25665b0d872ac0634cec76f38fc95
Sahab Yazdani <sahab@theworkinggroup.ca>
2017-01-04 11:59:01 -0500

    Generate a self-signed key for both Dev and initial Production setup.

--- chef/site-cookbooks/main/recipes/prod_setup.rb
+++ chef/site-cookbooks/main/recipes/prod_setup.rb
@@ -1,5 +1,14 @@
-include_recipe 'nodejs::nodejs_from_package'
+include_recipe 'nodejs'
 include_recipe 'monit'
+include_recipe 'openssl'
 
 package 'zip'
 package 'sqlite3'
+
+openssl_x509 '/var/db/localhost.crt' do
+  common_name 'localhost'
+  org 'My Portfolio'
+  org_unit ''
+  key_length 4096
+  country 'CA'
+end
