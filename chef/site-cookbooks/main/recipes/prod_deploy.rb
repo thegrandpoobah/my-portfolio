ce6f0436bd271b0891db46ac331301ed3453abf6
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-01-04 23:54:46 -0500

    Move CRON to Setup, and minor changes to CERTS

--- chef/site-cookbooks/main/recipes/prod_deploy.rb
+++ chef/site-cookbooks/main/recipes/prod_deploy.rb
@@ -68,28 +68,6 @@ execute 'restart my-portfolio' do
 	returns [0, 1]
 end
 
-cron 'store account mv' do
-	minute "00"
-	hour "23"
-	weekday "1-5"
-
-	program = [
-		"cd /srv/www/current",
-		"source /srv/www/shared/app.env",
-		"/usr/bin/env NODE_PATH=#/srv/www/current/node_modules:/srv/www/current /usr/bin/node cron/storeaccountmv.js >> /var/log/my-portfolio-cron.log 2>&1"
-	].join ' ; '
-
-	command "/bin/bash -c '#{program}'"
-end
-
 execute 'generate first certificate' do
-	command "/usr/bin/certbot certonly --webroot -w /srv/www/current/static -d #{node['domain']} --non-interactive --agree-tos --email #{node['email']} --rsa-key-size 4096 --post-hook \"ln --force --symbolic /etc/letsencrypt/live/#{node['domain']}/fullchain.pem /vol/db/localhost.crt; ln --force --symbolic /etc/letsencrypt/live/#{node['domain']}/privkey.pem /vol/db/localhost.key; monit restart my-portfolio\""
-end
-
-cron 'update ssl certificate' do
-	minute "27" # this should be random
-	hour "11,23"
-	weekday "*"
-
-	command "/usr/bin/certbot renew --quiet --no-self-upgrade --post-hook \"ln --force --symbolic /etc/letsencrypt/live/#{node['domain']}/fullchain.pem /vol/db/localhost.crt; ln --force --symbolic /etc/letsencrypt/live/#{node['domain']}/privkey.pem /vol/db/localhost.key; monit restart my-portfolio\""
+	command "/usr/bin/certbot certonly --webroot -w /srv/www/current/static -d #{node['cert_domain']} --non-interactive --agree-tos --email #{node['cert_email']} --rsa-key-size 4096 --post-hook \"ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/fullchain.pem /vol/db/localhost.crt; ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/privkey.pem /vol/db/localhost.key; monit restart my-portfolio\""
 end
--- chef/site-cookbooks/main/recipes/prod_setup.rb
+++ chef/site-cookbooks/main/recipes/prod_setup.rb
@@ -5,7 +5,15 @@ include_recipe 'openssl'
 package 'zip'
 package 'sqlite3'
 
-openssl_x509 '/var/db/localhost.crt' do
+file '/vol/db/localhost.crt' do
+  action :delete
+end
+
+file '/vol/db/localhost.key' do
+  action :delete
+end
+
+openssl_x509 '/vol/db/localhost.crt' do
   common_name 'localhost'
   org 'My Portfolio'
   org_unit ''
@@ -19,4 +27,26 @@ remote_file '/usr/bin/certbot' do
   group 'root'
   mode '0755'
   action :create
+end
+
+cron 'store account mv' do
+  minute "00"
+  hour "23"
+  weekday "1-5"
+
+  program = [
+    "cd /srv/www/current",
+    "source /srv/www/shared/app.env",
+    "/usr/bin/env NODE_PATH=#/srv/www/current/node_modules:/srv/www/current /usr/bin/node cron/storeaccountmv.js >> /var/log/my-portfolio-cron.log 2>&1"
+  ].join ' ; '
+
+  command "/bin/bash -c '#{program}'"
+end
+
+cron 'update ssl certificate' do
+  minute "27" # this should be random
+  hour "11,23"
+  weekday "*"
+
+  command "/usr/bin/certbot renew --quiet --no-self-upgrade --post-hook \"ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/fullchain.pem /vol/db/localhost.crt; ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/privkey.pem /vol/db/localhost.key; monit restart my-portfolio\""
 end
