d35c926da2a8d28018ae7b5ea07b1b10146238b5
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-10-24 00:50:50 -0400

    Fix up some problems in the chef script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf dist
 (cd ./client && ../node_modules/webpack/bin/webpack.js --progress --colors)
 cp ./server/db.js ./dist/db.js
 cp ./package.json ./dist/package.json
+cp ./server/blockchain.js ./dist/blockchain.js
 cp ./server/questrade.js ./dist/questrade.js
 cp ./server/server.js ./dist/server.js
 mkdir -p ./dist/cron && cp ./server/cron/storeaccountmv.js ./dist/cron/storeaccountmv.js
