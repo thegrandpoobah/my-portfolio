dc4c63be73304708ca4eaab5c0e17f9a51779e69
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2018-03-09 01:08:02 -0500

    Delete old server.zip file before making new one

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ cp ./server/questrade.js ./dist/questrade.js
 cp ./server/server.js ./dist/server.js
 mkdir -p ./dist/cron && cp ./server/cron/storeaccountmv.js ./dist/cron/storeaccountmv.js
 cp -R ./server/config ./dist/config
+rm server.zip
 (cd ./dist && zip -r ../server.zip ./*)
 aws s3 cp --region us-east-1 ./server.zip s3://my-portfolio-deploy
