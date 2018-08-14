898662fa1b11dd5d946f7b25bebe89824f24e26f
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2018-08-14 00:49:16 -0400

    More coherent error messaging

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ var DatabaseFile = config.get('database_uri')
 
 function connect () {
   if (!fs.existsSync(DatabaseFile)) {
-    throw new Error(`Database file ${DatabaseFile} does not exist. Please run migrate.js to create`)
+    throw new Error(`Database file ${DatabaseFile} does not exist. Please initialize the db before starting server`)
   }
 
   return new sqlite3.Database(DatabaseFile)
