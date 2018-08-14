1579aa4f87d38cd11b72dea929ba234e670d4902
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2018-08-13 23:56:53 -0400

    DRY on some necessary preamble

--- migrate.js
+++ migrate.js
@@ -1,124 +1,119 @@
 const _ = require('lodash')
+const fs = require('fs')
 const sqlite3 = require('sqlite3')
 const config = require('config')
 const log = require('npmlog')
 const Promise = require('bluebird')
 const DatabaseFile = config.get('database_uri')
 
-function createDbHistorySchema (db) {
+function migrate (db, fn) {
   return new Promise((resolve, reject) => {
-    log.info('migrations', 'Executing migration createDbHistorySchema')
-
     db.serialize(() => {
-      db.get('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'dbHistory\'', (err, row) => {
-        if (err) {
-          reject(err)
-
-          return
-        }
-
-        if (_.isUndefined(row)) {
-          log.info('migrations', 'Creating dbHistory table')
-
-          db.run('CREATE TABLE dbHistory (name TEXT)', (err) => {
-            if (err) {
-              reject(err)
-
-              return
-            }
-
-            resolve()
-          })
-        } else {
-          resolve()
-        }
-      })
+      fn(db, resolve, reject)
     })
   })
 }
 
-function createMvSchema (db) {
-  return new Promise((resolve, reject) => {
-    log.info('migrations', 'Executing migration createMvSchema')
+function createDbHistorySchema (db, resolve, reject) {
+  log.info('migrations', 'Executing migration createDbHistorySchema')
 
-    db.serialize(() => {
-      db.get('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'mv\'', (err, row) => {
+  db.get('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'dbHistory\'', (err, row) => {
+    if (err) {
+      reject(err)
+
+      return
+    }
+
+    if (_.isUndefined(row)) {
+      log.info('migrations', 'Creating dbHistory table')
+
+      db.run('CREATE TABLE dbHistory (name TEXT)', (err) => {
         if (err) {
           reject(err)
 
           return
         }
 
-        if (_.isUndefined(row)) {
-          log.info('migrations', 'Creating mv table')
-
-          db.serialize(() => {
-            db.run('CREATE TABLE mv (number INTEGER, date INTEGER, currency TEXT, cash TEXT, marketValue TEXT, cost TEXT)')
-            db.run('CREATE INDEX mv_fast ON mv (number, date)', err => {
-              if (err) {
-                reject(err)
-
-                return
-              }
-
-              resolve()
-            })
-          })
-        } else {
-          resolve()
-        }
+        resolve()
       })
-    })
+    } else {
+      resolve()
+    }
   })
 }
 
-function insertMvIntoHistory (db) {
-  return new Promise((resolve, reject) => {
-    log.info('migrations', 'Executing migration insertMvIntoHistory')
+function createMvSchema (db, resolve, reject) {
+  log.info('migrations', 'Executing migration createMvSchema')
 
-    db.serialize(() => {
-      db.get('SELECT name FROM dbHistory WHERE name=\'mvSchema\'', (err, row) => {
-        if (err) {
-          reject(err)
+  db.get('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'mv\'', (err, row) => {
+    if (err) {
+      reject(err)
 
-          return
-        }
+      return
+    }
 
-        if (_.isUndefined(row)) {
-          log.info('migrations', 'Adding mvSchema to history')
+    if (_.isUndefined(row)) {
+      log.info('migrations', 'Creating mv table')
 
-          db.run('INSERT INTO dbHistory VALUES (\'mvSchema\')', (err) => {
-            if (err) {
-              reject(err)
-            }
+      db.serialize(() => {
+        db.run('CREATE TABLE mv (number INTEGER, date INTEGER, currency TEXT, cash TEXT, marketValue TEXT, cost TEXT)')
+        db.run('CREATE INDEX mv_fast ON mv (number, date)', err => {
+          if (err) {
+            reject(err)
 
-            resolve()
-          })
-        } else {
+            return
+          }
+
           resolve()
-        }
+        })
       })
-    })
+    } else {
+      resolve()
+    }
   })
 }
 
-function createActivitiesSchema (db) {
-  return new Promise((resolve, reject) => {
-    log.info('migrations', 'Executing migration createActivitiesSchema')
+function insertMvIntoHistory (db, resolve, reject) {
+  log.info('migrations', 'Executing migration insertMvIntoHistory')
 
-    db.serialize(() => {
-      db.get('SELECT name FROM dbHistory WHERE name=\'activitiesSchema\'', (err, row) => {
+  db.get('SELECT name FROM dbHistory WHERE name=\'mvSchema\'', (err, row) => {
+    if (err) {
+      reject(err)
+
+      return
+    }
+
+    if (_.isUndefined(row)) {
+      log.info('migrations', 'Adding mvSchema to history')
+
+      db.run('INSERT INTO dbHistory VALUES (\'mvSchema\')', (err) => {
         if (err) {
           reject(err)
-
-          return
         }
 
-        if (_.isUndefined(row)) {
-          log.info('migrations', 'Adding activties tables')
+        resolve()
+      })
+    } else {
+      resolve()
+    }
+  })
+}
 
-          db.serialize(() => {
-            db.run(`CREATE TABLE activities (
+function createActivitiesSchema (db, resolve, reject) {
+  log.info('migrations', 'Executing migration createActivitiesSchema')
+
+  db.get('SELECT name FROM dbHistory WHERE name=\'activitiesSchema\'', (err, row) => {
+    if (err) {
+      reject(err)
+
+      return
+    }
+
+    if (_.isUndefined(row)) {
+      log.info('migrations', 'Adding activties tables')
+
+      db.serialize(() => {
+        db.run(`CREATE TABLE activities (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               accountNumber INTEGER,
               tradeDate INTEGER,
@@ -135,39 +130,53 @@ function createActivitiesSchema (db) {
               commission NUMERIC,
               netAmount NUMERIC,
               type TEXT)`)
-            db.run('CREATE INDEX activites_fast ON activities (accountNumber, tradeDate, symbol, symbolId)')
-            db.run('INSERT INTO dbHistory VALUES (\'activitiesSchema\')', err => {
-              if (err) {
-                reject(err)
+        db.run('CREATE INDEX activites_fast ON activities (accountNumber, tradeDate, symbol, symbolId)')
+        db.run('INSERT INTO dbHistory VALUES (\'activitiesSchema\')', err => {
+          if (err) {
+            reject(err)
 
-                return
-              }
+            return
+          }
 
-              resolve()
-            })
-          })
-        } else {
           resolve()
-        }
+        })
       })
-    })
+    } else {
+      resolve()
+    }
   })
 }
 
 log.info('migrations', 'Running Migrations')
 
+// if we are operating in testing mode, then load the backup if it exists
+// this way, we can keep retrying the last migration that we write over
+// and over again
+if (process.argv[2] === 'test' && fs.existsSync(DatabaseFile + '.bak')) {
+  fs.copyFileSync(DatabaseFile + '.bak', DatabaseFile)
+}
+
+// take a backup of the existing database just in case something goes
+// horribly wrong
+if (fs.existsSync(DatabaseFile)) {
+  fs.copyFileSync(DatabaseFile, DatabaseFile + '.bak')
+}
+
 const db = new sqlite3.Database(DatabaseFile)
 
-createDbHistorySchema(db)
-  .then(() => { return createMvSchema(db) })
-  .then(() => { return insertMvIntoHistory(db) })
-  .then(() => { return createActivitiesSchema(db) })
-  .then(() => {
-    db.close()
+Promise.map([
+  createDbHistorySchema,
+  createMvSchema,
+  insertMvIntoHistory,
+  createActivitiesSchema
+], (fn) => {
+  return migrate(db, fn)
+}, { concurrency: 1 }).then(() => {
+  db.close()
 
-    process.exit(0)
-  }).error(e => {
-    log.error('migrations', e)
+  process.exit(0)
+}).error(e => {
+  log.error('migrations', e)
 
-    process.exit(1)
-  })
+  process.exit(1)
+})
